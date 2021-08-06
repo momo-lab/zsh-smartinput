a824fe166f7ec9e9c29cf3c28661ad73ddb42fcf
momotaro <momotaro.n@gmail.com>
2021-08-06 14:49:45 +0900

    First commit.

---
+++ smartinput.plugin.zsh
@@ -0,0 +1,22 @@
+declare -gA __smartinput_rules=(
+  '(' ')'
+  '[' ']'
+  "\"" "\""
+  "'" "'"
+  '`' '`'
+)
+
+function __smartinput:widget:main {
+  if [[ "${LBUFFER[-1]}" != '\' ]]; then
+    RBUFFER="${__smartinput_rules[$KEYS]}$RBUFFER"
+  fi
+  zle self-insert
+}
+
+() {
+  zle -N __smartinput:widget:main
+  for key in ${(k)__smartinput_rules}; do
+    bindkey "$key" __smartinput:widget:main
+  done
+}
+
